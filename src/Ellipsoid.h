f066d90c8ba3f8a6f1b74e78650e8a1126864c35
OskarKasjaniuk <oskarkasjaniuk@gmail.com>
2026-03-17 22:34:46 +0100

    Final solution: fixed ShapeParam and ShapeResult accessors

--- src/Ellipsoid.h
+++ src/Ellipsoid.h
@@ -2,7 +2,7 @@
 #define _ELLIPSOID_H
 
 #include "Shape3D.h"
-#include "ShapeResult.h"
+#include "ShapeResultData.h"
 #include <string>
 #include <cmath>
 #include <iostream>
@@ -10,8 +10,10 @@
 
 using namespace std;
 
-template<class T>
-class Ellipsoid : public Shape3D<T> {
+template <class T> class Ellipsoid : public Shape3D<T> {
+private:
+    ShapeParam<T> localParam;
+
 public:
     inline virtual ShapeResult<T> compute();
 
@@ -19,34 +21,40 @@ public:
     inline Ellipsoid(const ShapeParam<T>& param);
 };
 
-template<class T>
-inline ShapeResult<T> Ellipsoid<T>::compute() {
+template <class T> inline ShapeResult<T> Ellipsoid<T>::compute()
+{
     ShapeResult<T> results;
     const T PI = static_cast<T>(3.141592653589793);
 
-    T a = this->param.values[0];
-    T b = this->param.values[1];
-    T c = this->param.values[2];
+    T a = localParam.get_attrib(static_cast<ShapeParamIndex>(0));
+    T b = localParam.get_attrib(static_cast<ShapeParamIndex>(1));
+    T c = localParam.get_attrib(static_cast<ShapeParamIndex>(2));
 
-    results.volume = (static_cast<T>(4) / 3) * PI * a * b * c;
+    T volume = (static_cast<T>(4) / 3) * PI * a * b * c;
 
     T p = static_cast<T>(1.6075);
     T term1 = pow(a * b, p);
     T term2 = pow(a * c, p);
     T term3 = pow(b * c, p);
+    T surfaceArea = static_cast<T>(4) * PI
+        * pow((term1 + term2 + term3) / static_cast<T>(3),
+              static_cast<T>(1.0) / p);
 
-    results.surfaceArea = 4 * PI * pow((term1 + term2 + term3) / 3, 1.0 / p);
+    results.set_attrib(static_cast<ShapeResultIndex>(0), volume);
+    results.set_attrib(static_cast<ShapeResultIndex>(1), surfaceArea);
 
     return results;
 }
 
-template<class T>
-inline string Ellipsoid<T>::print() {
-    return "Bryla: Elipsoida. Obliczono objetosc i pole powierzchni calkowitej.";
+template <class T> inline string Ellipsoid<T>::print()
+{
+    return "Bryla: Elipsoida. Obliczono objetosc i pole powierzchni "
+           "calkowitej.";
 }
 
-template<class T>
-inline Ellipsoid<T>::Ellipsoid(const ShapeParam<T>& param) : Shape3D<T>(param) {
-}
+template <class T>
+inline Ellipsoid<T>::Ellipsoid(const ShapeParam<T>& param)
+    : Shape3D<T>(param), localParam(param)
+{}
 
 #endif
